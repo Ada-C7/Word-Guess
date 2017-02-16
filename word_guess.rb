a518e0e577248bd95c9381238709c0a1b7b54750
Lynn Trickey <lynn.trickey@gmail.com>
2017-02-16 15:34:01 -0800

    Created dictionary hash, defined classes

--- word_guess.rb
+++ word_guess.rb
@@ -1,4 +1,54 @@
-puts "What letter would you like to guess?"
-guess = gets.chomp
+# #Classes
+# class Dictionary
+#
+#   initialize(dictionary_hash)
+#     @type: dictionary_hash[:type]
+#   end
+#
+# end
 
-puts "Your guess is #{ guess }."
+class Word
+  attr_reader :name
+
+  def initialize(word)
+    @word = word
+
+  end
+
+#   def choose_word(word, number_of_letters)
+#
+#   end
+#
+# end
+#
+# class letter
+#
+# end
+#
+# class Marquee
+#
+# end
+#
+# class Art
+#
+# end
+#
+# #Methods
+# def trigger
+#
+# end
+print "What type of word would you like to guess?"
+type = gets.chomp.to_sym
+
+dictionary_hash = {
+  sports: ["ball", "basket"],
+  foods: ["apple", "soup"]
+}
+
+dictionary_hash[type].sample
+
+#
+# print "What letter would you like to guess?\r"
+# guess = gets.chomp
+#
+# print "Your guess is #{ guess }.\r"
