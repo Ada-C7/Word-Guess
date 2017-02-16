668263382e9af8f84d2b5bc98fac37815b0b5945
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-02-16 15:08:45 -0800

    Yay! working pieces.

--- word-guess.rb
+++ word-guess.rb
@@ -1,1 +1,47 @@
-"testing"
+# define class
+class  WordGame
+attr_accessor :answer, :answer_array, :empty_array
+
+  def initialize
+    @answer = "cat"
+    @guess
+    @answer_array = @answer.split("")
+    @empty_array = Array.new(@answer_array.length, " _ ")
+  end
+
+  # gets guess from user
+  def guess
+    puts "Enter guess: "
+    @guess = gets.chomp
+  end
+
+  # compares the user's letter to answer array
+  def evaluate
+    @answer_array.each do |letter|
+      if @answer_array.include?(@guess)
+        return "correct!"
+      else
+        return "wrong"
+      end
+    end
+  end
+#
+#   # method if answer is wrong
+#   def incorrect
+#
+#   end
+#
+#   # method if answer is correct
+#   def correct
+#
+#   end
+#
+end
+
+word = WordGame.new
+puts word.answer
+print word.answer_array
+print word.empty_array
+
+puts word.guess
+puts word.evaluate
