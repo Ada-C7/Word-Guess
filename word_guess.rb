ff9541d8d0f69b78ce0d53edadcc47770d93fe83
Theresa Manney <theresa.m.17@gmail.com>
2017-02-16 14:50:52 -0800

    Outline for program

---
+++ word_guess.rb
@@ -0,0 +1,20 @@
+# Word guess game created by Alena Messmer and Theresa Manney
+# Started on 2.16.17
+
+# Create game class
+class WordGame
+  # Method initialize
+
+  # Method Ascii art
+
+  # Method word work... prints correct and incorect guesses along with underscores
+
+  # Input evaluation
+
+
+
+end
+
+# Create an array that will contain words
+
+# Create user prompt
