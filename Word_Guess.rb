54a87722f2a593f25242679e64148d5194c8b386
Olivia <ssecretsharer@gmail.com>
2017-02-16 15:32:56 -0800

    Finished stripped down version. Start class Game.

--- Word_Guess.rb
+++ Word_Guess.rb
@@ -1,4 +1,52 @@
-puts "What's the magic word? Let's guess a letter."
+#correct boolean
+# define class Turn and begin to elaborate methods
+class Game
+  attr_accessor :win?
+
+  def initialize (word_array)
+    @win?
+    @guess
+    @counter
+    @word_array
+
+  end
+
+  def turn
+  end
+
+  def check_guess
+  end
+
+  def redraw_letter_blanks
+  end
+
+  def update_counter
+  end
+
+  def redraw_art
+  end
+
+end
+#library
+
+library = ["HAI", "HAIRCUT", "POTATO"]
+
+#select word from library
+limit = library.length
+
+chosen_word = library[rand(limit)]
+word_array = chosen_word.chars
+puts chosen_word
+puts word_array
+#ask user for guess
+
+puts "What's your letter guess?"
 guess1 = gets.chomp.upcase
 
-puts guess1
+#compare guess against word/report correct or incorrect
+
+if chosen_word.include?(guess1)
+  puts "true"
+else
+  puts "false"
+end
