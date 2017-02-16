ba57790d93235b760ed946051f233f27c9fd513b
Emma Claire Philippides <acme9898@gmail.com>
2017-02-16 15:48:59 -0800

    initial structure

---
+++ word-guess.rb
@@ -0,0 +1,34 @@
+class GamePlay
+
+  def initialize
+    #call update_image
+    #call generate_word method
+  end
+
+  def generate_word
+    #use faker gem to create word
+    #split the letters into an array, save as instance variable
+  end
+
+  def update_image (#Incorrect guess)
+    #initial image with all 5 flowers
+    #case when flowers = 0 statement (ex. 5 flowers)
+    # flowers -= 5
+    # flowers =4, output 4.txt
+  end
+
+  def check_letters?(guess )
+    #check if true or false
+  end
+
+  def add_to_correct_guess_array
+    #adds all our correct guesses, to the correct guess array
+  end
+
+  def add_to_incorrect_guess_array
+
+  end
+
+  def update_result
+
+  end
