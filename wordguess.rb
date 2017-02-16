0aab4b257b77b662d6cdbe9686cdd2305a3063fd
Anna Elisabeth <anna.e.boyd@gmail.com>
2017-02-16 15:56:41 -0800

    added word generator array

--- wordguess.rb
+++ wordguess.rb
@@ -1,8 +1,9 @@
 class WordGuess
+  attr_reader = :word
 
   def initialize
     @word = gen_word
-    @word_as_dashes = word_dash
+    @word_as_dashes = nil
     @word_as_array = #word_dash_array   #not defined yet
     puts intro
   end
@@ -10,9 +11,12 @@ class WordGuess
 # show ascii, generate word, show dashes
   def intro
     puts "Welcome!"
+    puts gen_word
   end
 
   def gen_word
+   array_of_words = ["pug", "stumptown", "normcore", "pitchfork", "chillwave", "cardigan"]
+    @word = array_of_words[rand(0..5)]
   end
 
   def word_dash
