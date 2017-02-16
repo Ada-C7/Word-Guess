8d86a39f88b4a248f1807520f77f5269f3551d4b
Anacelia Gillette <acgillette426@gmail.com>
2017-02-16 15:14:32 -0800

    Added functionality to User class

--- wordguess.rb
+++ wordguess.rb
@@ -1,9 +1,42 @@
+require 'terminal-table'
+
 class User
-  attr_accessor :difficulty, :letter_guessed
+  attr_accessor :difficulty, :letter_guessed, :word_to_guess
 
   def initialize
     @difficulty
     @letter_guessed
+    @word_to_guess
+    puts "Welcome to Word Guess!"
+    prompt
+  end
+
+  def prompt
+    rows = []
+    rows << ["Easy", 10]
+    rows << ["Medium", 7]
+    rows << ["Hard", 5]
+    table = Terminal::Table.new :title => "Pick your Difficulty",
+    :headings => ["Difficulty", "Num of Guesses"], :rows => rows
+    puts table
+    input = gets.chomp.downcase
+    case input
+    when "easy"
+      @difficulty = 10
+    when "medium"
+      @diffculty = 7
+    when "hard"
+      @difficulty = 5
+    else
+      puts "That's not a valid input"
+      prompt
+    end
+    word
+  end
+
+  def word
+    options = ["cast", "puppy", "pineapple", "bananas"]
+    @word_to_guess = options.sample
   end
 
 
@@ -11,7 +44,14 @@ end
 
 class GamePlay
 
-  def initialize
+  def initialize(user)
+
+  end
+
+  def graphic
   end
 
 end
+
+user = User.new
+game = GamePlay.new(user)
