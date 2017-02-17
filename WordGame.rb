97113e973e02f350c5c13f501be55d0dff24ae33
Danielle Birbal <birbalds@gmail.com>
2017-02-16 16:23:14 -0800

    Added user input limitations

--- WordGame.rb
+++ WordGame.rb
@@ -4,23 +4,35 @@ class Word
     attr_reader :theword
 
     def initialize
-        @theword = Faker::Cat.name
+        @theword = 'ada' # Faker::Cat.name.downcase
     end
 
     def dashes
         @theword.length.to_i.times do
+            @answer = []
+            @answer << '_'
             print '_ '
         end
     end
 
     def guess
+        @letter = ''
+
+        until ('a'..'z').cover?(@letter)
+            print "\nPlease guess a letter! (a..z): "
+            @letter = gets.chomp.downcase
+        end
+
         @userguess = []
-        @userguess << gets.chomp
+        @userguess << @letter
         puts 'You have guessed ' + @userguess.to_s + 'thus far'
         @gameword = @theword.split('')
 
         if @gameword.include?(@userguess.last)
-            puts 'yeee'
+
+            @gameword.each do |i|
+                @answer[i.to_i] = @userguess.last if @theword[i.to_i] == @userguess.last
+            end
         else
             puts 'prolly broken'
         end
