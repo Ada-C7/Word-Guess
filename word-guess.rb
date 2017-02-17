19531b0e2fed0b574eb729c5aaed86224cf9a4ef
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-02-17 14:23:26 -0800

    colorized

--- word-guess.rb
+++ word-guess.rb
@@ -1,21 +1,23 @@
 require 'faker'
+require 'colorize'
 
 # define class
 class  WordGame
-  attr_accessor :answer_array, :empty_array, :wrong_guesses
-  attr_reader :answer
+ attr_accessor :answer_array, :empty_array, :wrong_guesses, :dead_flower
+ attr_reader :answer
 
-  def initialize
-    @answer = Faker::Cat.name.upcase
-    @guess
-    @answer_array = @answer.split("")
-    @empty_array = Array.new(@answer_array.length, " _ ")
-    @wrong_guesses = []
-    @flower = ["(@)", "(@)", "(@)", "(@)", "(@)"]
-  end
+def initialize
+   @answer = Faker::Cat.name.upcase
+   @guess
+   @answer_array = @answer.split("")
+   @empty_array = Array.new(@answer_array.length, " _ ")
+   @wrong_guesses = []
+   @flower = ["(@)", "(@)", "(@)", "(@)", "(@)"]
+   @dead_flower = []
+ end
 
-  # gets guess from user
-  def run_game
+# gets guess from user
+ def run_game
     while @wrong_guesses.length < 5 && @answer_array != @empty_array
       check_input
       evaluate
@@ -25,7 +27,7 @@ class  WordGame
     end_game
   end
 
-  # evaluates whether the user's guess was a valid input
+# evaluates whether the user's guess was a valid input
   def check_input
       print "Guess: "
       @guess = gets.chomp.upcase
@@ -34,87 +36,87 @@ class  WordGame
         check_input
       end
       while @wrong_guesses.include?(@guess) || @empty_array.include?(@guess)
-        puts "You've already guesses that!"
+        puts "You've already guessed that!"
         check_input
       end
   end
 
-  def select_level
-    print "Select level [1] EASY, [2] MEDIUM, or [3] HARD: "
-    level = gets.chomp.downcase
+def select_level
+   print "Select level [1] EASY, [2] MEDIUM, or [3] HARD: "
+   level = gets.chomp.downcase
 
-    if level == "1" || level == "easy"
+  if level == "1" || level == "easy"
 
-    elsif level == "2" || level == "medium"
+  elsif level == "2" || level == "medium"
 
-    elsif level == "3" || level == "hard"
+  elsif level == "3" || level == "hard"
 
-    else
-      select_level
-    end
-  end
+  else
+     select_level
+   end
+ end
 
+# makes sure user hasn't already guessed that letter
+ # evaluates the user's guess as correct or incorrect
+ def evaluate
+   if @answer_array.include?(@guess)
+     correct
+   else
+     incorrect
+   end
+ end
 
-  # makes sure user hasn't already guessed that letter
-  # evaluates the user's guess as correct or incorrect
-  def evaluate
-    if @answer_array.include?(@guess)
-      correct
-    else
-      incorrect
-    end
-  end
+# displays correct letter
+ def correct
+   index_array = @answer_array.each_index.select {|index| @answer_array[index] == @guess}
 
-  # displays correct letter
-  def correct
-    index_array = @answer_array.each_index.select {|index| @answer_array[index] == @guess}
+  index_array.each do |index|
+     @empty_array[index] = @guess
+   end
+ end
 
-    index_array.each do |index|
-      @empty_array[index] = @guess
-    end
-  end
+# removes a flower if answer is incorrect
+ def incorrect
+   @wrong_guesses << @guess
+   @flower.pop
+   @dead_flower << "(@)"
+ end
 
-  # removes a flower if answer is incorrect
-  def incorrect
-    @wrong_guesses << @guess
-    @flower.pop
-  end
+# displays how many petals are left
+ # displays blank spaces and correct letters
+ # display wrong guesses
+ def display
+   print "\n    " + @flower.join("").light_magenta + @dead_flower.join("").light_black.blink
+   puts "\n   \,\\,\\,|,/,/,".light_green
+   puts "      _\\|/_".light_green
+   puts "     |_____|".light_cyan
+   puts "      |   |".light_cyan
+   puts "      |___|\n\n".light_cyan
+   puts "Word: " + @empty_array.join(" ")
+   puts "\nWrong Guesses: #{@wrong_guesses.join(" ").white.on_red.blink} \n"
+ end
 
-  # displays how many petals are left
-  # displays blank spaces and correct letters
-  # display wrong guesses
-  def display
-    print "\n    " + @flower.join("")
-    puts "\n   \,\\,\\,|,/,/,"
-    puts "      _\\|/_"
-    puts "     |_____|"
-    puts "      |   |"
-    puts "      |___|\n\n"
-    puts "Word: " + @empty_array.join(" ")
-    puts "\nWrong Guesses: #{@wrong_guesses.join(" ")}\n"
-  end
+# tells user whether they won or lost
+ # if they lost displays word
+ # prompts user to play again
+ def end_game
+   if @answer_array == @empty_array
+     puts "CONGRATULATIONS! You guessed the correct word.".blue.on_light_green.blink
+   else
+     puts "YOU RAN OUT OF GUESSES. The correct word was #{@answer}."
+   end
 
-  # tells user whether they won or lost
-  # if they lost displays word
-  # prompts user to play again
-  def end_game
-    if @answer_array == @empty_array
-      puts "CONGRATULATIONS! You guessed the correct word."
-    else
-      puts "YOU RAN OUT OF GUESSES. The correct word was #{@answer}."
-    end
+  print "\nWould you like to play again? (YES/NO): "
+   response = gets.chomp.downcase
 
-    print "\nWould you like to play again? (YES/NO): "
-    response = gets.chomp.downcase
-
-    if response == "yes"
-      new_game = WordGame.new
-      puts new_game.run_game
-    else
-      puts "GOODBYE!"
-      exit
-    end
-  end
+  if response == "yes"
+     new_game = WordGame.new
+     puts new_game.run_game
+   else
+     puts "GOODBYE!"
+     exit
+   end
+ end
 end
 
 # displays title
