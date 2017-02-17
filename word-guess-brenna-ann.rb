79befd0495d3f63feda2088b9771f4546605f1d9
Ann Dai <gofarann@gmail.com>
2017-02-16 16:09:21 -0800

    made guess_right and guess_wrong methods

--- word-guess-brenna-ann.rb
+++ word-guess-brenna-ann.rb
@@ -10,46 +10,67 @@ class SecretWord
     else
       @word = word.upcase
       @length = word.length
-      @word_array = word.upcase.scan /\w/
     end
   end
 
-  # def to_array
-  #   @word_array = @word.scan /\w/
-  #
-  # end
+  def to_array
+    @word.scan /\w/
+  end
 
 end
 
 
 class Guess
-  attr_reader :word, :user_input
+  attr_reader :letter, :right_array, :wrong_array
 
-  def initialize (word, user_input)
-    @word = word
-    @user_input = user_input
+  def initialize (word_array, letter)
+    @word_array = word_array
+    @letter = letter
+    @right_array = Array.new(word_array.length)
+    @wrong_array = Array.new(word_array.length)
   end
 
+  def guess_right
+    # checks guessed letter for frequency in word
+
+
+# below only adds the first index the letter appears in the word
+    index = nil
+    @word_array.each do |character|
+      if character == @letter
+        index = @word_array.index(character)
+         @right_array.insert(index, @letter)
+      end
+    end
+    print @right_array
+    puts "right"
+  end
+
+  def guess_wrong
+    wrong_array << @letter
+  end
+
+  def win
+  end
+
+  def lose
+  end
 
 end
 
 
+
 word1 = SecretWord.new
+puts word1.word
+puts word1.to_array
 puts "give me a letter"
-user_input = gets.chomp.upcase
-guess1 = Guess.new(word1, user_input)
+user_letter = gets.chomp.upcase
+guess1 = Guess.new(word1.to_array, user_letter)
 
 #this is the conditional for guessing
-if word1.word_array.include?(user_input)
-  puts "right"
+if word1.to_array.include?(user_letter)
+  guess1.guess_right
 else puts "wrong"
 # elsif puts "win"
 # else puts "lose"
 end
-
-
-
-puts word1.word
-
-puts word1.length
-puts word1.word_array[0]
