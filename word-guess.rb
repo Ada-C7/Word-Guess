0390e3571a38ccbf8fbf7454704d5d2fbb9ef955
Emma Claire Philippides <acme9898@gmail.com>
2017-02-16 16:57:52 -0800

    check letters

--- word-guess.rb
+++ word-guess.rb
@@ -3,33 +3,31 @@ class GamePlay
   attr_reader :word_array
 
   def initialize
-    @guess = gets.chomp
+    @letter_guess = gets.chomp
     word = Faker::Color.color_name
-    @word_array = word.scan /\w/
-
-    print @word_array
+   @word_array = word.scan /\w/
+    print @word_array #for testing
 
     #call update_image
-    #call generate_word method
+    #call generate_word methods
   end
-end
-  # def update_word
-  #   #use faker gem to create word
-  #   #split the letters into an array, save as instance variable
-  #   #display the generated word in dashes (word.length * "-")
-  # end
-  #
-  # def update_image (#Incorrect guess)
-  #   #initial image with all 5 flowers
-  #   #case when flowers = 0 statement (ex. 5 flowers)
-  #   # flowers -= 5
-  #   # flowers =4, output 4.txt
-  # end
-  #
-  # def check_letters?(guess)
-  #   #check if true or false
-  # end
-  #
+# end
+#   def update_word
+#     #display the generated word in dashes (word.length * "-")
+#   end
+#
+#   def update_image (#Incorrect guess)
+#     #initial image with all 5 flowers
+#     #case when flowers = 0 statement (ex. 5 flowers)
+#     # flowers -= 5
+#     # flowers =4, output 4.txt
+#   end
+
+  def check_letters?
+
+    puts @word_array.any? { |guess_match| @letter_guess.include?(guess_match)}
+  end
+
   # def add_to_correct_guess_array
   #   #adds all our correct guesses, to the correct guess array
   # end
@@ -42,10 +40,10 @@ end
   #   #first go through words check, then update flower
   #   #check_letters(guess)
   #   #if te
-  #
-  #
-  # end
-  #
+
+
+  end
+
   # File.open(file_name) do |f|
   # f.each_line do |line|
   #   #I added in the colorize gem so if you want color use line 4, else you can omit
@@ -53,3 +51,4 @@ end
   #   print line
   # end
 new_game = GamePlay.new
+new_game.check_letters?
