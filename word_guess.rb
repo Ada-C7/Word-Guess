67a90d0a7fe1bb569c91ee2ddfe986f77e00081c
amb54 <ambarklund@hotmail.com>
2017-02-17 14:56:39 -0800

    Added methods to check_input:  initialize @ word; added methods to Flower:  pot, print_flower, update ascii,

--- word_guess.rb
+++ word_guess.rb
@@ -1,9 +1,18 @@
+require 'random_word_generator'
+require "colorize"
+
 
 class Word
+  # require 'random_word_generator'
+
+  attr_reader :word, :word_in_progress, :guessed_letters
+
   def initialize
-    @word = ["C", "A", "T"]
-    @word_in_progress = ["C", "_", "T"]
-    @guessed_letters = ["D", "F", "C"]
+    @word = RandomWordGenerator.of_length(5).upcase.split("")
+    @word_in_progress = ["_", "_", "_", "_", "_"]
+    @guessed_letters = []
+    puts "TEST #{@word}"
+
   end
 
 
@@ -20,51 +29,65 @@ class Word
         # update_flower
         puts "TEST Here we are going to update flowers"
       end
+    elsif input.length > 1
+      won?(input.split(""))
+      puts "TEST Here we are going to update flowers"
+    end
 
-      if input.length > 1
-        won?
-        return true
-      else
-        return false
-      end
+  end
 
+def update_guessed_letters(input)
+  if !@guessed_letters.include? input
+    @guessed_letters << input
+    puts "TEST Uppdated list #{@guessed_letters.join(", ")}"
+  end
+end
+
+
+def update_word_in_progress(letter)
+  @word.length.times do |index|
+    if @word[index] == letter
+      @word_in_progress[index] = letter
     end
   end
+  puts "TEST Uppdated word in progress #{@word_in_progress.join(" ")}"
+end
 
-  def update_guessed_letters(input)
-    if !@guessed_letters.include? input
-      @guessed_letters << input
-      puts "TEST Uppdated list #{@guessed_letters.join(", ")}"
-    end
+def won?(word_guess)
+  if word_guess == @word
+    puts "#{@word.join} is correct. You win!!!"
+    exit
+  else
+    false
   end
+end
 
-
-  def update_word_in_progress(letter)
-    @word.length.times do |index|
-      if @word[index] == letter
-        @word_in_progress[index] = letter
-      end
-    end
-    puts "TEST Uppdated word in progress #{@word_in_progress.join(", ")}"
-  end
-
-  def won?(word_guess)
-    if word_guess == @word
-      puts "#{@word.join} is correct. You win!!!"
-      exit
-    else
-      return false
-    end
-  end
-
-  def print_word
-  end
+def print_word
+end
 
 end
 
 class Flower
   def initialize
+    @flowers = [5,4,0]
   end
+
+  def pot
+    puts "  ,\\,\\,|,/,/,".colorize(:green)
+    puts "     _\\|/_".colorize(:green)
+    puts "    |_____|".colorize(:blue)
+    puts "     |   |".colorize(:blue)
+    puts "     |___|".colorize(:blue)
+  end
+
+  def print_flower
+    puts "\n   #{"(@)" * @flowers[2]}".colorize(:red)
+    puts " #{"(@)" * @flowers[1]}".colorize(:red)
+    puts "#{"(@)" * @flowers[0]}".colorize(:red)
+    pot
+  end
+
+
 
   def lost?
   end
@@ -73,10 +96,15 @@ class Flower
   end
 
   def update_ascii
+    if @flowers[2] > 0
+      @flowers[2] -= 1
+    elsif @flowers[1] > 0
+      @flowers[1] -= 1
+    elsif flowers[0] > 0
+      @flowers[0] -= 1
+    end
   end
 
-  def print_ascii
-  end
 end
 
 
@@ -88,6 +116,11 @@ end
 # test.update_guessed_letters(input)
 # test.update_word_in_progress(input)
 # test.check_input(input)
+
+test_flower = Flower.new
+test_flower.print_flower
+test_flower.update_ascii
+test_flower.print_flower
 
 def main
 
