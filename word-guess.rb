d2ea5a2ba0bfabc147dc2e22d3bfb93e43dbdc8b
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-02-16 16:20:37 -0800

    Double letters working

--- word-guess.rb
+++ word-guess.rb
@@ -1,57 +1,57 @@
 # define class
 class  WordGame
-  attr_accessor :answer, :answer_array, :empty_array, :wrong_guesses
+ attr_accessor :answer, :answer_array, :empty_array, :wrong_guesses
 
-  def initialize
-    @answer = "cat"
-    @guess
-    @answer_array = @answer.split("")
-    @empty_array = Array.new(@answer_array.length, " _ ")
-    @wrong_guesses = []
-  end
+def initialize
+   @answer = "hello"
+   @guess
+   @answer_array = @answer.split("")
+   @empty_array = Array.new(@answer_array.length, " _ ")
+   @wrong_guesses = []
+ end
 
-  # gets guess from user
-  def guess
-    while @wrong_guesses.length < 5 && @answer_array != @empty_array
-      puts "Enter guess: "
-      @guess = gets.chomp
-      evaluate
-      guess
-    end
-  end
+# gets guess from user
+ def guess
+   while @wrong_guesses.length < 5 && @answer_array != @empty_array
+     puts "Enter guess: "
+     @guess = gets.chomp
+     evaluate
+     guess
+   end
+ end
 
-  # compares the user's letter to answer array
-  def evaluate
-    if @answer_array.include?(@guess)
-      correct
-      puts "correct!"
-      puts @empty_array
-    else
-      incorrect
-      puts "incorrect"
-    end
-  end
+# compares the user's letter to answer array
+ def evaluate
+   if @answer_array.include?(@guess)
+     correct
+     puts "correct!"
+     puts @empty_array
+   else
+     incorrect
+     puts "incorrect"
+   end
+ end
 
-  # method if answer is correct
-  def correct
-    @answer_array.each do |letter|
-      #find index of answer_array
-      if letter == @guess
-        index = @answer_array.index(@guess)
-        @empty_array[index] = @guess
-        # puts @empty_array
-      end
-    end
-  end
-  #
-  # method if answer is wrong
-  # penalize if wrong
-  # take away a petal
-  # display wrong guesses
+# method if answer is correct
+ def correct
+   index = @answer_array.each_index.select {|index| @answer_array[index] == @guess}
 
-  def incorrect
-    @wrong_guesses << @guess
-  end
+  index.each do |index|
+       @empty_array[index] = @guess
+   end
+   # puts @empty_array
+ end
+ #
+ # method if answer is wrong
+ def incorrect
+   @wrong_guesses << @guess
+ end
+
+end
+
+# take away a petal
+# display wrong guesses
+def display
 
 end
 
