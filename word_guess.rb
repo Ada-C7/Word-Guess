57dfd2baf841e852043a9178204b5f49e3c6724f
Theresa Manney <theresa.m.17@gmail.com>
2017-02-16 22:12:03 -0800

    Fixed all sorts of bugs, this program now runs with a flower pot that looses its (@) for every miss-guessed letter

--- word_guess.rb
+++ word_guess.rb
@@ -5,56 +5,99 @@ require 'faker'
 # Create game class
 class WordGame
   attr_reader :word
+  attr_writer :letters
+  attr_accessor :progress, :wrong_guesses, :max_guesses
+
+
   # Method initialize
   def initialize(unique_word)
-    @letters = []
+
     @word = unique_word
-    @max_guesses
+    @max_guesses = 15
+    @wrong_guesses = []
+    @progress = Array.new(unique_word.length, "_")
   end
-  puts @word
-  # Method Ascii art
-  def print_ascii_art
+end
+def run_game
+  # word_game = start(words)
+  # word_game = start(words)
+  words = create_word_bank
+  word_game = start(words)
+  #prompt_user(word_game.progress)
 
+
+  until word_game.max_guesses == 0
+    roses = pruning_roses(word_game.max_guesses)
+    puts print_ascii_art(roses)
+    guess = prompt_user(word_game.progress)
+    input_evaluation(guess, word_game)
+    print word_game.wrong_guesses
+    puts
   end
-  # Method word work... prints correct and incorect guesses along with underscores
-  def word_work
-    underscores = []
-    underscores = @word.upcase.split(//)
+  print word_game.progress
+  print word_game.word
+end
 
-  end
 
-  #add letter method
-  def add_letter(letter)
-    @letters << letter
+def print_ascii_art(roses)
+  #roses = "(@)(@)(@)(@)(@)\n"
+  pot = "\n,\\,\\,|,/,/,
+  _\\|/_
+  |_____|
+  |   |
+  |___|
+  "
+  full_pot = roses + pot
+end
 
-  end
-
-  # Input evaluation
-  def input_evaluation
-    #while
-
-    #end
-  end
+def pruning_roses(max)
+  roses = "(@)(@)(@)(@)(@)"
+  roses[0...max]
 
 end
 
-# Create an array that will contain words
-words = []
+# Input evaluation
+def input_evaluation(guess, correct_word)
+  if correct_word.word.include?(guess)
+    correct_word.word.each_with_index do |letter, index|
+      if letter == guess
+        correct_word.progress[index] = guess
+
+      end
+    end
+  else
+    correct_word.wrong_guesses << guess
+    correct_word.max_guesses -= 3
+  end
+end
 
 # BUGZ: color might be greater than one work, how to handle spaces?
-10.times do
-  words << Faker::Color.unique.color_name
+def create_word_bank
+  words = []
+  10.times do
+    words << Faker::Color.unique.color_name
+  end
+  return words
 end
 
-game_word = WordGame.new(words[0])
+def start(array)
+  #gsub is a string method that can take regex as an argument, in this case it will strip all spaces
+  #print array
+  word = array[0].gsub(/\s+/,"").chars
+  WordGame.new(word)
+end
 
-puts game_word.word
-# puts words
-print game_word.word_work
+# puts game_word.word
+# # puts words
+# print game_word.word_work
 
 # Create user prompt
-puts "Hello user, please guess a letter > "
-letter_guess = gets.chomp.upcase
-puts game_word.add_letter(letter_guess)
+def prompt_user(game_board)
+  print game_board
 
-#letter_guess.input_evaluation
+  puts "\nHello user, please guess a letter > "
+  gets.chomp
+
+
+end
+run_game
