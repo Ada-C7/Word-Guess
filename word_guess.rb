af6c27f7f4e9811360d8a9e3a9ac2bf57717c7d9
Lynn Trickey <lynn.trickey@gmail.com>
2017-02-17 13:32:14 -0800

    Created Method of getting word

--- word_guess.rb
+++ word_guess.rb
@@ -1,107 +1,45 @@
 # #Classes
-# class Dictionary
+# class Marquee
+#   attr_accessor :number
 #
+#   def initialize(sampled_word)
+#     @number = sampled_word.name.length
+#   end
 #
-#   initialize(dictionary_hash)
-#     @type = dictionary_hash[:type]
+#   def create_marquee()
+#     print "_" * @number
 #   end
 #
 # end
 
-class Word
- attr_accessor :name
-
- def initialize (word)
-   @name = word
- end
-
- def create_new_game()
-   create_marquee
-
- end
-
-def sample_word(type, type_hash)
-end
-
- sampled_word = Word.new(dictionary_hash[type].sample)
-
- def check_letter(guess)
-   if @name.include?(guess.name)
-     "Yes, this letter is in this word."
-     #Letter shows up on Marquee
-   else
-     "Nope, guess again, lose a life."
-     #Something happens to Art
-   end
- end
-
-end
-
-#   def choose_word(word, number_of_letters)
-#
-#   end
-#
-# end
-#
-class Letter
-attr_accessor :name
-
-  def initialize(letter)
-    @name = letter
-  end
-
-end
-
-
-
-class Marquee
-  attr_accessor :number
-
-  def initialize(sampled_word)
-    @number = sampled_word.name.length
-  end
-
-  def create_marquee()
-    print "_" * @number
-  end
-
-end
-
-# class Art
-#
-# end
 #
 #Methods
-# def trigger
-#   #asses guess, modify Art & Marquee
-#
-# end
+def get_word
 
-print "What type of word would you like to guess?"
-type = gets.chomp.to_sym
+while true
+  #promts user to enter a type of word they
+  #want to guess
+  print "What type of word would you like to guess?\n"
+  number = 1
+  dictionary_hash = {
+   sports: ["ball", "basket", "hoop", "stadium", "tennis", "gymnastics", "curling", "badminton", "referee", "athlete"],
+   food: ["apple", "soup", "breakfast", "sprinkles", "picnic", "pancake", "marshmallow", "horseradish", "cinnamon", "artichoke"]
+  }
+  #lists the types of food words to choose from
+  dictionary_hash.keys.each do |type|
+    print "#{ number }. #{ type.capitalize }\n"
+    number += 1
+  end
+  type = gets.chomp.downcase.to_sym
+  #makes sure their type is included in our list
+  if dictionary_hash.keys.include?(type)
+    #generates the word to guess
+    game_word = (dictionary_hash[type].sample)
+    puts game_word
+    break
+  else
+    puts "That's not a type of word we can use.  The types are: "
+  end
+end
 
-
-
-puts sampled_word.name.length
-
-game_marquee = Marquee.new
-puts game_marquee
-
-# #Prompts player to make a guess
-# puts "What letter would you like to guess?"
-# letter = gets.chomp.downcase
-#
-# guess = Letter.new(letter)
-# #
-# puts guess.class
-# puts sampled_word.check_letter(guess)
-#
-# # print "What letter would you like to guess?\r"
-# guess = gets.chomp
-#
-# print "Your guess is #{ guess }.\r"
-
-# dictionary_hash = {
-#  sports: ["ball", "basket"],
-#  foods: ["apple", "soup"]
-# }
+end
