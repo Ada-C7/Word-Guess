4eeab506305e85cce099c6a5d3c98846ae68dd96
amb54 <ambarklund@hotmail.com>
2017-02-17 11:26:06 -0800

    Methods added to class Word  won? update_word_in_progress update_guessed_letters check_input(incomlete)

--- word_guess.rb
+++ word_guess.rb
@@ -1,26 +1,60 @@
 
 class Word
   def initialize
-    @word = []
-    @word_in_progress = []
+    @word = ["C", "A", "T"]
+    @word_in_progress = ["C", "_", "T"]
+    @guessed_letters = ["D", "F", "C"]
   end
 
 
-  def check_letter(letter)
-  end
+  def check_input(input)
 
-  def update_word_in_progress(letter)
-  end
+    if input.length == 1
+      update_guessed_letters(input)
 
-  def won?
-    if @word == @word_in_progress
-      return true
-    else
-      return false
+      if @word.include? input
+        update_word_in_progress(input)
+        won?(@word_in_progress)
+        puts "TEST Not won yet "
+      else
+        # update_flower
+        puts "TEST Here we are going to update flowers"
+      end
+
+      if input.length > 1
+        won?
+        return true
+      else
+        return false
+      end
+
     end
   end
 
-  def check_word
+  def update_guessed_letters(input)
+    if !@guessed_letters.include? input
+      @guessed_letters << input
+      puts "TEST Uppdated list #{@guessed_letters.join(", ")}"
+    end
+  end
+
+
+  def update_word_in_progress(letter)
+    @word.length.times do |index|
+      if @word[index] == letter
+        @word_in_progress[index] = letter
+      end
+    end
+    puts "TEST Uppdated word in progress #{@word_in_progress.join(", ")}"
+  end
+
+  def won?(word_guess)
+    if word_guess == @word
+      puts "#{@word.join} is correct. You win!!!"
+      exit
+    else
+      return false
+    end
   end
 
   def print_word
@@ -43,9 +77,17 @@ class Flower
 
   def print_ascii
   end
-
 end
 
+
+# test = Word.new
+#
+# print "Enter letter"
+# input = gets.chomp.upcase
+
+# test.update_guessed_letters(input)
+# test.update_word_in_progress(input)
+# test.check_input(input)
 
 def main
 
