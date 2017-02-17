50da6df144eebe727f9c0c1197458593c534a38f
Janice Lichtman <jalichtman@gmail.com>
2017-02-16 16:51:02 -0800

    working includes printing dashes and guessed letters

--- word_guess.rb
+++ word_guess.rb
@@ -4,7 +4,18 @@ class WordGuess
 
   def initialize
     @correct_word =  ["potato", "america", "flower"].sample
+
+    @correct_letter_array = []
+
+    @correct_word.each_char do |letter|
+      if !(@correct_letter_array )
+        @correct_letter_array << letter
+      end
+    end
+
+
     @used_letters = []
+
     puts "Ready to have some word guessing fun? Try to guess the word before all the birds fly away. Let the games begin!"
 
     puts "This is the initial art!"
@@ -14,30 +25,33 @@ class WordGuess
     turn
   end
 
+    def turn
+      #WILL NOT BE IN REAL games
+      puts "secret word = "+ @correct_word
+      #######
 
-  def turn
-    #WILL NOT BE IN REAL games
-    puts "secret word = "+ @correct_word
-    
-    puts "Please choose a letter:"
-
-    guess = gets.chomp.downcase
-
-    until (guess.length == 1) && all_letters(guess)
-      puts "C'mon. that's not a letter. What letter would you like to guess?"
+      puts "Please choose a letter:"
       guess = gets.chomp.downcase
+
+      #Testing that user input is a single valid letter
+      until (guess.length == 1) && guess[/[a-zA-Z]+/] == guess
+        puts "C'mon. that's not a letter. What letter would you like to guess?"
+        guess = gets.chomp.downcase
+      end
+
+      @used_letters << guess
+
+      @correct_word.each_char do |n|
+        if @used_letters.include?(n)
+          print n
+        else
+          print "_ "
+        end
+      end
+      puts
     end
 
-    used_letters << guess
-    @correct_word.each_char do |n|
-      if used_letters.include(n)
-        puts n
-      else
-        puts "Sorry, the letter #{n} is not in the word."
-      end
-    end
   end
 
-end
 
 game_one = WordGuess.new
