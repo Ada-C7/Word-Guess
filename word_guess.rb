c0a6b15ff4ce95e6076bcdb397bde3382f4835b0
Janice Lichtman <jalichtman@gmail.com>
2017-02-17 12:41:49 -0800

    cleaner printing of letters already guessed

--- word_guess.rb
+++ word_guess.rb
@@ -28,92 +28,95 @@ class WordGuess
     end
   end
 
-    #puts the letters of the given word into an array without duplicates
-    def  build_correct_letter_array
-      @correct_word.each_char do |letter|
-        if !(@correct_letter_array.include?(letter))
-          @correct_letter_array << letter
-        end
+  #puts the letters of the given word into an array without duplicates
+  def  build_correct_letter_array
+    @correct_word.each_char do |letter|
+      if !(@correct_letter_array.include?(letter))
+        @correct_letter_array << letter
+      end
+    end
+  end
+
+
+  def begin_game
+    while @correct_letter_array != []
+      turn
+    end
+    "Congratulations! You guesst it! Your lilly pad is hopping."
+    continue_game
+  end
+
+
+
+  def turn
+    #WILL NOT BE IN REAL games
+    puts "secret word = "+ @correct_word
+    #######
+
+    puts "Please choose a letter:"
+    guess = gets.chomp.downcase
+
+    #Testing that user input is a single valid letter
+    until (guess.length == 1) && guess[/[a-zA-Z]+/] == guess
+      puts "C'mon. that's not a letter. What letter would you like to guess?"
+      guess = gets.chomp.downcase
+    end
+
+    @used_letters << guess
+
+    puts
+    @correct_word.each_char do |n|
+      if @used_letters.include?(n)
+        print n
+        @correct_letter_array.delete(n)
+      else
+        print "_ "
       end
     end
 
-
-      def begin_game
-        while @correct_letter_array != []
-          turn
-        end
-        "Congratulations! You guesst it! Your lilly pad is hopping."
-        continue_game
+    if !(@correct_word.include?(guess))
+      if !(@wrong_letter_array.include?(guess))
+        @wrong_letter_array << guess
       end
+    end
+    puts
+    puts "The wrong letters guessed are:"
+    @wrong_letter_array.each do |n|
+      print n
+    end
+    puts
 
-
-
-      def turn
-        #WILL NOT BE IN REAL games
-        puts "secret word = "+ @correct_word
-        #######
-
-        puts "Please choose a letter:"
-        guess = gets.chomp.downcase
-
-        #Testing that user input is a single valid letter
-        until (guess.length == 1) && guess[/[a-zA-Z]+/] == guess
-          puts "C'mon. that's not a letter. What letter would you like to guess?"
-          guess = gets.chomp.downcase
-        end
-
-        @used_letters << guess
-
-        @correct_word.each_char do |n|
-          if @used_letters.include?(n)
-            print n
-            @correct_letter_array.delete(n)
-          else
-            print "_ "
-          end
-        end
-
-        if !(@correct_word.include?(guess))
-          if !(@wrong_letter_array.include?(guess))
-             @wrong_letter_array << guess
-          end
-        end
-        puts
-        puts "The wrong letters guessed are:"
-        print  @wrong_letter_array
-        puts
-
-        if game_end?
-          puts "Sorry, you used up all your guesses. All the frigs have leapt away and you are sitting aolne at an empty pond."
-          continue_game
-        end
-
-      end
-
-
-      def continue_game
-        print "Do you want to keep playing? (Y to play / N to quit):"
-        answer = gets.chomp.downcase
-        until answer == "y"|| answer =="n"
-          print "Enter Y/N: "
-          answer = gets.chomp.downcase
-        end
-        if answer == "n"
-          exit
-        else
-          new_game = WordGuess.new
-        end
-      end
-
-
-
-      private
-
-      def game_end?
-        @wrong_letter_array.length == 5
-      end
-
+    if game_end?
+      puts "Sorry, you used up all your guesses. All the frigs have leapt away and you are sitting aolne at an empty pond."
+      continue_game
     end
 
+  end
 
-    game_one = WordGuess.new
+
+  def continue_game
+    print "Do you want to keep playing? (Y to play / N to quit):"
+    answer = gets.chomp.downcase
+    until answer == "y"|| answer =="n"
+      print "Enter Y/N: "
+      answer = gets.chomp.downcase
+    end
+    if answer == "n"
+      exit
+    else
+      new_game = WordGuess.new
+    end
+  end
+
+
+
+  private
+
+  def game_end?
+    @wrong_letter_array.length == 5
+  end
+
+end
+
+
+game_one = WordGuess.new
