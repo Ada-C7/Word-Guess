dd4b021428dd66c971b7fc881939fe27c1323bec
Lynn Trickey <lynn.trickey@gmail.com>
2017-02-16 17:10:51 -0800

    moving name choices to methods

--- word_guess.rb
+++ word_guess.rb
@@ -15,6 +15,26 @@ class Word
    @name = word
  end
 
+ def create_new_game()
+   create_marquee
+
+ end
+
+def sample_word(type, type_hash)
+end
+
+ sampled_word = Word.new(dictionary_hash[type].sample)
+
+ def check_letter(guess)
+   if @name.include?(guess.name)
+     "Yes, this letter is in this word."
+     #Letter shows up on Marquee
+   else
+     "Nope, guess again, lose a life."
+     #Something happens to Art
+   end
+ end
+
 end
 
 #   def choose_word(word, number_of_letters)
@@ -24,46 +44,64 @@ end
 # end
 #
 class Letter
+attr_accessor :name
 
   def initialize(letter)
-
+    @name = letter
   end
 
 end
 
-# class Marquee
-#
-# end
-#
+
+
+class Marquee
+  attr_accessor :number
+
+  def initialize(sampled_word)
+    @number = sampled_word.name.length
+  end
+
+  def create_marquee()
+    print "_" * @number
+  end
+
+end
+
 # class Art
 #
 # end
 #
-# #Methods
+#Methods
 # def trigger
+#   #asses guess, modify Art & Marquee
 #
 # end
 
 print "What type of word would you like to guess?"
 type = gets.chomp.to_sym
 
-dictionary_hash = {
- sports: ["ball", "basket"],
- foods: ["apple", "soup"]
-}
 
-sample = Word.new(dictionary_hash[type].sample)
 
-puts sample.name
+puts sampled_word.name.length
 
-puts "What letter would you like to guess?"
-letter = gets.chomp.downcase
+game_marquee = Marquee.new
+puts game_marquee
 
-guess = Letter.new(letter)
-
-puts guess
+# #Prompts player to make a guess
+# puts "What letter would you like to guess?"
+# letter = gets.chomp.downcase
 #
-# print "What letter would you like to guess?\r"
+# guess = Letter.new(letter)
+# #
+# puts guess.class
+# puts sampled_word.check_letter(guess)
+#
+# # print "What letter would you like to guess?\r"
 # guess = gets.chomp
 #
 # print "Your guess is #{ guess }.\r"
+
+# dictionary_hash = {
+#  sports: ["ball", "basket"],
+#  foods: ["apple", "soup"]
+# }
