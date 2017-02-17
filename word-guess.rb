cb63d096b94469a850cf3376348123da080f0cbd
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-02-17 13:49:50 -0800

    checks user input

--- word-guess.rb
+++ word-guess.rb
@@ -5,7 +5,7 @@ class  WordGame
   attr_accessor :answer_array, :empty_array, :wrong_guesses
   attr_reader :answer
 
- def initialize
+  def initialize
     @answer = Faker::Cat.name.upcase
     @guess
     @answer_array = @answer.split("")
@@ -14,27 +14,32 @@ class  WordGame
     @flower = ["(@)", "(@)", "(@)", "(@)", "(@)"]
   end
 
- # gets guess from user
+  # gets guess from user
+  # evaluates whether the user's guess was a valid input
   def run_game
     while @wrong_guesses.length < 5 && @answer_array != @empty_array
       print "Guess: "
       @guess = gets.chomp.upcase
       until /[A-Z]/.match(@guess) && @guess.length == 1
-          puts "Please enter a single letter!"
-          print "Guess: "
-          @guess = gets.chomp.upcase
+        wrong_input
+      end
+      while @wrong_guesses.include?(@guess) || @empty_array.include?(@guess)
+        wrong_input
       end
       evaluate
       display
-      puts "\n"
       run_game
     end
     end_game
   end
 
+  def wrong_input
+    puts "Please enter a single letter!"
+    print "Guess: "
+    @guess = gets.chomp.upcase
+  end
 
- # *** evaluates whether the user's guess was a valid input
-  # *** makes sure user hasn't already guessed that letter?
+  # makes sure user hasn't already guessed that letter
   # evaluates the user's guess as correct or incorrect
   def evaluate
     if @answer_array.include?(@guess)
@@ -44,22 +49,22 @@ class  WordGame
     end
   end
 
- # displays correct letter
+  # displays correct letter
   def correct
     index_array = @answer_array.each_index.select {|index| @answer_array[index] == @guess}
 
-   index_array.each do |index|
+    index_array.each do |index|
       @empty_array[index] = @guess
     end
   end
 
- # removes a flower if answer is incorrect
+  # removes a flower if answer is incorrect
   def incorrect
     @wrong_guesses << @guess
     @flower.pop
   end
 
- # displays how many petals are left
+  # displays how many petals are left
   # displays blank spaces and correct letters
   # display wrong guesses
   def display
@@ -70,10 +75,10 @@ class  WordGame
     puts "      |   |"
     puts "      |___|\n\n"
     puts "Word: " + @empty_array.join(" ")
-    puts "\nWrong Guesses: #{@wrong_guesses.join(" ")}"
+    puts "\nWrong Guesses: #{@wrong_guesses.join(" ")}\n"
   end
 
- # tells user whether they won or lost
+  # tells user whether they won or lost
   # if they lost displays word
   # prompts user to play again
   def end_game
@@ -83,12 +88,12 @@ class  WordGame
       puts "YOU RAN OUT OF GUESSES. The correct word was #{@answer}."
     end
 
-   print "\nWould you like to play again? (YES/NO): "
+    print "\nWould you like to play again? (YES/NO): "
     response = gets.chomp.downcase
 
-   if response == "yes"
-        new_game = WordGame.new
-        puts new_game.run_game
+    if response == "yes"
+      new_game = WordGame.new
+      puts new_game.run_game
     else
       puts "GOODBYE!"
       exit
