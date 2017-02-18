ab8b092f10ee55e063e8869cb51f9863d226366c
Sahana Murthy <s.murthy730@gmail.com>
2017-02-17 16:07:51 -0800

    Included final methods

--- word_guess.rb
+++ word_guess.rb
@@ -1,4 +1,4 @@
-require "faker"
+require "random_word_generator"
 
 class Game
   attr_accessor :word, :user_guess, :image
@@ -7,36 +7,83 @@ class Game
     @word = word
     @word_array = []
     @user_guess = user_guess
-    @image = image
+    @guess_array = []
+    @correct = []
+    @total = 0
   end
 
   def generate_word
-    @word = Faker::HarryPotter.unique.character.downcase
+    @word = RandomWordGenerator.word.downcase
     puts "The word is #{@word}."
-
     @word_array = @word.chars.to_a
-
-    print @word_array
+    # print "#{@word_array}\n"
   end
 
   def display_dashes
-
     @word_array.each do |i|
       print "_ "
     end
   end
 
   def image
+    File.open('./candle_6.txt') do |f|
+      f.each_line do |line|
+        puts line
+      end
+    end
+    puts
+    puts
+    puts
   end
 
   def guess
+    print "\nPlease guess a letter: "
+    @user_guess = gets.chomp.downcase
+    if @guess_array.include?(@user_guess)
+      puts "You already guessed that!"
+    else
+      @guess_array.push(@user_guess)
+      @correct.push(@user_guess)
+    end
+  end
+
+  def update_dashes
+    # puts "inside update_dashes()"
+      @word_array.each do |i|
+        if i == @user_guess || @correct.include?(i)
+          print i
+        else
+          print "_ "
+        end
+      end
   end
 
   def verify
+    if @word_array.include?(@user_guess)
+      puts  "\nYou guessed correctly!"
+    else
+      puts "\nPlease try again"
+
+      # Tried to use this to end the game.
+      # @total++
+      #
+      # if @total == 6
+      #   puts "Sorry, you lost! :("
+      #   break
+      # end
+    end
   end
 
 end
 
+puts "Hey user! Welcome to Allison and Sahana's Harry Potter name hangman game!"
+
 hangman = Game.new
 hangman.generate_word
 hangman.display_dashes
+
+6.times do
+  hangman.guess
+  hangman.update_dashes
+  hangman.verify
+end
