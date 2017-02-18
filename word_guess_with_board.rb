0701a8354b0d55b1ccdbdacc785cbaee4ec5a8c5
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-02-17 16:04:46 -0800

    formatting pretty

--- word_guess_with_board.rb
+++ word_guess_with_board.rb
@@ -25,32 +25,34 @@ class Board
     @word.length.times do
       @spaces << "__"
     end
-    puts "created initial spaces"
+    # puts "created initial spaces"
 
   end
 
   def display_board
 
-    # DISPLAY pond
+    # DISPLAY POND
+    puts
     @pond.each do |space|
       print space + "  "
     end
     puts
-    puts "displaying pond"
+    # puts "displaying pond"
 
     # DISPLAY SPACES
+    puts
     @spaces.each do |space|
-      print space + "\t"
+      print space + "  "
     end
     puts
-    puts "displaying spaces"
+    # puts "displaying spaces"
 
   end
 
   def update_board(counter, player_guess)
 
     # UPDATE POND
-    puts "updating pond"
+    # puts "updating pond"
 
     @pond.length.times do |n|
       if n < counter || n > counter
@@ -70,14 +72,14 @@ class Board
     # end
 
     # UPDATE SPACES
-    puts "before updating spaces, spaces is #{@spaces}"
+    # puts "before updating spaces, spaces is #{@spaces}"
     word_array = @word.split("")
     word_array.each_with_index do |letter, index |
       if letter == player_guess
         @spaces[index] = letter
       end
     end
-    puts "after updating, spaces is #{@spaces}"
+    # puts "after updating, spaces is #{@spaces}"
   end
 
 end
@@ -100,14 +102,20 @@ class Game
     @letters_guessed = []
     @spaces
     @board = Board.new(@word, @player_guess)
-    puts "word is #{@word}."
+    # puts "word is #{@word}."
+    welcome_message
+  end
+
+  def welcome_message
+    puts "\nWelcome to Frog Hop!"
+    puts "Guess the correct letters in the word before the frog hops off your screen.\n\n"
     run_turn
   end
 
 
   def display_guessed_letters
     if @letters_guessed.length >0
-      print "You have guessed: "
+      print "\nYou have guessed: "
       @letters_guessed[0..-2].each do |letter|
         print "#{letter}, "
       end
@@ -124,13 +132,13 @@ class Game
   end
 
   def continue_or_quit
-    print "Would you like to play again? (yes/no) "
+    print "\nWould you like to play again? (yes/no) "
     user_response = gets.chomp
     case user_response
     when "yes"
       @word = @words.sample
       @letters_guessed = []
-      puts "word is #{@word}."
+      # puts "word is #{@word}."
       @board = Board.new(@word, @player_guess)
       run_turn
     when "no"
@@ -142,7 +150,7 @@ class Game
 
 
   def get_player_input
-    print "What is your guess? "
+    print "\nPlease guess a letter: "
     @player_guess = gets.chomp
     puts "You guessed: #{@player_guess}."
     verify_guess_is_new
@@ -163,7 +171,7 @@ class Game
   end
 
   def check_if_guess_is_right
-    puts "counter is now #{@counter}"
+    # puts "counter is now #{@counter}"
 
     if !@word.include? @player_guess
       @counter += 1
@@ -174,15 +182,15 @@ class Game
 
   def check_if_game_over
     if @counter == 5
-      puts "you ran out of guesses"
+      puts "The frog got away! You lose. Wah-wah."
       continue_or_quit
     end
     if @board.spaces.join == @word
-      puts "you win!"
+      puts "You win!"
       @board.display_board
       continue_or_quit
     end
-    puts "checked game over status and no result"
+    # puts "checked game over status and no result"
   end
 
 
