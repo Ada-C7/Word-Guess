bb212d48d6e90819f372d5964544e17f1f1fab1a
Emma Claire Philippides <acme9898@gmail.com>
2017-02-17 16:05:42 -0800

    added ASCII files

--- word-guess.rb
+++ word-guess.rb
@@ -1,21 +1,17 @@
 require 'faker'
 #require 'ASCII'
 class GamePlay
-  attr_reader :word_array, :letter_guess, :word_placeholders, :incorrect_guess
+  attr_reader :word_array, :letter_guess, :word_placeholders, :incorrect_guess, :art
 
   def initialize
     puts "Enter a letter:"
     @letter_guess = gets.chomp
-    word = "green" #[Faker::Color.color_name, Faker::Hacker.noun, Faker::Hacker.verb].sample
+    word = [Faker::Color.color_name, Faker::Hacker.noun, Faker::Hacker.verb].sample
     @word_array = word.scan /\w/
     @word_placeholders = ("- "*@word_array.length).split(" ")
     @incorrect_guess = []
-    @chances_left = 5
-    # print @word_placeholders
-    # print @word_array #for testing
-
-    #call update_image
-    #call generate_word methods
+    @chances_left = 4
+    @art = "4-guesses.txt"
   end
 
   def update_word
@@ -25,56 +21,68 @@ class GamePlay
         @word_placeholders[index] = letter
       end
     end
-      p @word_placeholders
-    # return @word_placeholders
-  end
-  # end
-  #
-  #   def update_image (#Incorrect guess)
-  #     #initial image with all 5 flowers
-  #     #case when flowers = 0 statement (ex. 5 flowers)
-  #     # flowers -= 5
-  #     # flowers =4, output 4.txt
-  #   end
-
-  def check_letters?
-    #checks if guessed letters are in our word
-    return @word_array.any? { |guess_match| @letter_guess.include?(guess_match)}
+    p @word_placeholders
   end
 
-  def add_to_incorrect_guess_array
-    @incorrect_guess.push(@letter_guess)
-  end
-
-  def update_result #passage of time #private?
-    until @chances_left == 0 do
-      if check_letters?
-        puts "Good job!"
-        update_word
-        @word_placeholders
-      else
-        puts "Wrong guess"
-        add_to_incorrect_guess_array
-        puts "These are all your wrong guesses #{@incorrect_guess}"
-        @chances_left -= 1
-        #remove a chance from ASCII
+  def display_image
+    File.open(@art) do |f|
+      f.each_line do |line|
+        print line
       end
-      puts "Enter another letter:"
-      @letter_guess = gets.chomp
     end
-    puts "Sorry! You weren't able to guess the right word."
-    exit
   end
-end
+
+      def check_letters?
+        #checks if guessed letters are in our word
+        return @word_array.any? { |guess_match| @letter_guess.include?(guess_match)}
+      end
+
+      def add_to_incorrect_guess_array
+        @incorrect_guess.push(@letter_guess)
+      end
+
+      def update_result #passage of time #private?
+        until @chances_left == 0 do
+          if check_letters?
+            puts "Good job!"
+            update_word
+            if !@word_placeholders.include?("-")
+              puts "Congratulations! You've guessed the word."
+              exit
+            end
+          else
+            puts "Wrong guess"
+            add_to_incorrect_guess_array
+            puts "These are all your wrong guesses #{@incorrect_guess}"
+            @chances_left -= 1
+            #remove a chance from ASCII
+          end
+          case @chances_left
+          when 4
+            @art = "4-guesses.txt"
+            display_image
+          when 3
+            @art = "3-guesses.txt"
+            display_image
+          when 2
+            @art = "2-guesses.txt"
+            display_image
+          when 1
+            @art = "1-guesses.txt"
+            display_image
+          end
+          puts "Enter another letter:"
+          @letter_guess = gets.chomp
+        end
+        puts "Sorry! You weren't able to guess the right word."
+        exit
+      end
+    end
 
 
-# File.open(file_name) do |f|
-# f.each_line do |line|
-#   #I added in the colorize gem so if you want color use line 4, else you can omit
-#   line = line.colorize(:cyan)
-#   print line
-# end
-new_game = GamePlay.new
-#new_game.update_word
-# new_game.check_letters?
-new_game.update_result
+
+    new_game = GamePlay.new
+    #new_game.update_word
+    # new_game.check_letters?
+    new_game.update_result
+    # new_game.display_image
